0f59d0cc829c42f5e8bed6a493ede9287a821bb0
waynesinclair-rsa <wsi@netactive.co.za>
2021-02-06 20:23:29 +0200

    First commit

---
+++ test/TestRunner.java
@@ -0,0 +1,22 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import java.util.Iterator;
+
+public class TestRunner {
+    public TestRunner() {
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(new Class[]{BookingTest.class});
+        Iterator i = result.getFailures().iterator();
+
+        while(i.hasNext()) {
+            Failure failure = (Failure)i.next();
+            System.out.println(failure.toString());
+        }
+
+        System.out.println("Result==" + result.wasSuccessful());
+    }
+}
